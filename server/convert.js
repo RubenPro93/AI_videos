a8aa1b1e374320bc17a6d31e2d3ae209ae7b5768
RubenPro93 <ruben.alves.t0123774@edu.atec.pt>
2023-10-14 22:33:17 +0100

    commit

---
+++ server/convert.js
@@ -0,0 +1,42 @@
+import fs from "fs"
+
+//converter para wav
+import wav from "node-wav"
+
+//manipular o audio
+import ffmpeg from "fluent-ffmpeg"
+
+//apontar para apontar a biblioteca de cima
+import ffmpegStatic from "ffmpeg-static"
+
+const filePath = "./tmp/audio.mp4"
+const outputPath = filePath.replace(".mp4", ".wav")
+
+export const convert = () =>
+  new Promise((resolve, reject) => {
+    console.log("Convertendo o vídeo...")
+
+    ffmpeg.setFfmpegPath(ffmpegStatic)
+    ffmpeg()
+      .input(filePath)
+      .audioFrequency(16000)
+      .audioChannels(1)
+      .format("wav")
+      .on("end", () => {
+        const file = fs.readFileSync(outputPath)
+        const fileDecoded = wav.decode(file)
+
+        const audioData = fileDecoded.channelData[0]
+        const floatArray = new Float32Array(audioData)
+
+        console.log("Vídeo convertido com sucesso!")
+
+        resolve(floatArray)
+        fs.unlinkSync(outputPath)
+      })
+      .on("error", (error) => {
+        console.log("Erro ao converter o vídeo", error)
+        reject(error)
+      })
+      .save(outputPath)
+  })
